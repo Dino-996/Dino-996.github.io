4a3f5fb917da8d09bd69c8b44d2bc293703eccf0
Dino-996 <davidesabia22@gmail.com>
2023-07-20 22:40:31 +0200

    Aggiornamento dell'interfaccia utente

---
+++ src/js/rechapta.js
@@ -0,0 +1,8 @@
+"use strict"
+
+function onClick(e) {
+    e.preventDefault();
+    grecaptcha.enterprise.ready(async () => {
+        const token = await grecaptcha.enterprise.execute('6LfJwjQnAAAAADlLvNSbyiYPnfJK7lH_Lp0qteL6', { action: 'LOGIN' });
+    });
+}
