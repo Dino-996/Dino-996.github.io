55fdb68be708450f571f01783110d05f208825ca
Dino-996 <davidesabia22@gmail.com>
2023-10-15 17:04:37 +0200

    Update

--- dist/index.2f15edf4.js
+++
@@ -1,3 +0,0 @@
-document.querySelector("#copy").innerHTML=`&copy; ${new Date().getFullYear()}`,document.querySelector("#devYear").innerHTML=`${new Date().getFullYear()-2020}`;//# sourceMappingURL=index.2f15edf4.js.map
-
-//# sourceMappingURL=index.2f15edf4.js.map
