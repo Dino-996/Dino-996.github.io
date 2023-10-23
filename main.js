54274ff7b8081acc4ce5fee2865969445964feec
Dino-996 <davidesabia22@gmail.com>
2023-10-23 20:32:36 +0200

    Upgrade

---
+++ main.js
@@ -0,0 +1,41 @@
+'use strict'
+
+const THANK_WRITING = 'Grazie per avermi scritto, risponderò nel più breve tempo possibile';
+const THANK_VISIT = 'Grazie per aver visitato il sito web';
+
+function getMail(email) {
+    const a = document.createElement('a');
+    const message = document.createElement('h6');
+    const hr = document.createElement('hr');
+    message.style.color = 'whitesmoke';
+    message.style.marginTop = '1em';
+    a.href = `mailto:${email}`;
+    a.click();
+    const writingMessage = document.createTextNode(THANK_WRITING);
+    message.appendChild(writingMessage);
+    message.appendChild(hr);
+    document.getElementById('message').appendChild(message);
+    setTimeout(() => {
+        message.removeChild(writingMessage);
+        message.removeChild(hr);
+    }, 10000);
+}
+
+function openURL(url) {
+    const a = document.createElement('a');
+    const message = document.createElement('h6');
+    const hr = document.createElement('hr');
+    message.style.color = 'whitesmoke';
+    message.style.marginTop = '1em';
+    a.href = `https://www.${url}`;
+    a.target = '_blank';
+    a.click();
+    const visitMessage = document.createTextNode(THANK_VISIT);
+    message.appendChild(visitMessage);
+    message.appendChild(hr);
+    document.getElementById('message').appendChild(message);
+    setTimeout(() => {
+        message.removeChild(visitMessage);
+        message.removeChild(hr);
+    }, 10000);
+}
